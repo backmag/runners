e9139fd83b377cae1d6ba9096bcf7d1d68c51e66
Gustaf Backman <gustaf.backman@spp.se>
2025-03-09 13:01:42 +0100

    Update vite deps

--- runners-frontend/src/App.jsx
+++ runners-frontend/src/App.jsx
@@ -18,8 +18,8 @@ function App() {
           </div>
         </header>
         <main>
-          <div className="max.w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-            <div className="pf-4 py-6 sm:px-0">
+          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            <div className="px-4 py-6 sm:px-0">
               <Chart />
             </div>
           </div>
